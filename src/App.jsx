073581854d80b192906e8f399f70d7f620c78a09
marcmillie <mrmarcusmiller0@gmail.com>
2026-05-25 17:27:38 -0400

    added pages home and favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import './App.css'
-import MovieCard from "./components/MovieCard"
+
 
 function App() {
-  const movieNumber = 1;
-
   return (
     <>
-      {movieNumber === 1 ? (
-        <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
-      ) : (
-        <MovieCard movie={{title: "Joes Film", release_date: "2020"}}/>
-      )}
+     
     </>
   )
 }
