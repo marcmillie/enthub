2ac5f9527166b6371261bd4eca0e18d6365fa878
marcmillie <mrmarcusmiller0@gmail.com>
2026-05-25 17:39:46 -0400

    home jsx updated with search bar input and btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,18 @@ function Home() {
     {id: 3, title: "The Matrix", release_date: "1998"},
   ]
   
+  const handleSearch = () => {};
   
   return <div className="home">
+    <form onSubmit={handleSearch} className="search-form">
+      <input 
+        type="text" 
+        placeholder="Search for movies..." 
+        className="search-input"
+      />
+      <button type="search" className="search-btn">Search</button>
+    </form>
+
     <div clasName="movies-grid">
       {movies.map((movie) => (
         <MovieCard movie={movie} key={movie.id}/>
